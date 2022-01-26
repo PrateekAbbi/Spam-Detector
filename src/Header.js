0db9aa95695bd46324b6808fbb985a6d41b4d056
PrateekAbbi <68960673+PrateekAbbi@users.noreply.github.com>
2022-01-26 13:56:21 +0530

    Changed layout

--- src/Header.js
+++ src/Header.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-    StyleSheet,
-    View,
-    Text,
-    Linking,
-    StyleProp,
-    TextStyle,
-    ViewStyle,
-  } from 'react-native';
-  import { Appbar } from 'react-native-paper';
-  import { Platform } from 'react-native';
+  StyleSheet,
+  View,
+  Text,
+  Linking,
+  StyleProp,
+  TextStyle,
+  ViewStyle,
+} from 'react-native';
+import {Appbar} from 'react-native-paper';
+import {Platform} from 'react-native';
 
+const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
 
-  const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
-
-  class Header extends Component {
-    render() {
-      return (
-        <Appbar.Header >
-       <Appbar.Content titleStyle={{fontWeight:'bold', color:'#262424'}} title="Spam Detector" color="#000"  />
+class Header extends Component {
+  render() {
+    return (
+      <Appbar.Header>
+        <Appbar.Content
+          titleStyle={{fontWeight: 'bold', color: '#262424'}}
+          title="Spam Detector"
+          color="#000"
+        />
         {/* <Appbar.Action icon="magnify" color="#fff" onPress={() => {}} /> */}
         <Appbar.Action icon={MORE_ICON} color="#000" onPress={() => {}} />
-    </Appbar.Header>
-      );
-    }
+      </Appbar.Header>
+    );
   }
+}
 
-  export default Header;
+export default Header;
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,41 +1,56 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {View, Text, Image, Touchable} from 'react-native';
 import {
-    View,
-    Text,
-    Image,
-    Touchable
-} from 'react-native';
-import {
-    Card, Title, Paragraph,
-    Avatar,
-    Badge,
-    IconButton,
-    TouchableRipple,
-    Divider,
+  Card,
+  Title,
+  Paragraph,
+  Avatar,
+  Badge,
+  IconButton,
+  TouchableRipple,
+  Divider,
 } from 'react-native-paper';
 
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
-const Messages = (props) => {
-    console.log(props.data._id);
-    return (
-        <Card>
-            {/* <Divider /> */}
-            <TouchableRipple
-                onPress={() => { alert(props.data.body) }}
-                rippleColor="rgba(0, 0, 0, .32)"
-            >
-                <Card.Title titleStyle={{ color: "grey",fontWeight:'bold',  marginLeft:0  }}
-                    title={props.data.address}
-                    subtitle={props.data.body}
-                    // left={(props) => <Icon {...props} size={30} style={{ paddingLeft:7, paddingTop:4, color: '#F32013',backgroundColor:'#b5b3b3',  borderRadius:50, height:40, width:40}} name="exclamation-circle" />}
-                    right={(props) => <Icon {...props} size={30} style={{ marginRight:8 , paddingLeft:0, paddingTop:4, color: '#F32013',  borderRadius:50, height:40, width:40}} name="exclamation-circle" />}
-                    
-                />
-            </TouchableRipple>
-            {/* <Icon name="exclamation" size={30} color="#900" /> */}
-        </Card>
-    );
-}
+const Messages = props => {
+  console.log(props.data._id);
+  return (
+    <Card>
+      {/* <Divider /> */}
+      <TouchableRipple
+        onPress={() => {
+          alert(props.data.body);
+        }}
+        rippleColor="rgba(0, 0, 0, .32)">
+        <Card.Title
+          titleStyle={{color: 'grey', fontWeight: 'bold', marginLeft: -20}}
+          title={props.data.address}
+          subtitleStyle={{marginLeft: -20}}
+          subtitle={props.data.body}
+          // left={(props) => <Icon {...props} size={30} style={{ paddingLeft:7, paddingTop:4, color: '#F32013',backgroundColor:'#b5b3b3',  borderRadius:50, height:40, width:40}} name="exclamation-circle" />}
+          left={props => (
+            <Icon
+              {...props}
+              size={25}
+              style={{
+                marginRight: 5,
+                paddingLeft: 0,
+                paddingTop: 8,
+                color: '#F32013',
+                borderRadius: 50,
+                height: 40,
+                width: 40,
+              }}
+              name="exclamation-circle"
+            />
+          )}
+          right={props => <Text style={{marginRight: 15}}>5:50</Text>}
+        />
+      </TouchableRipple>
+      {/* <Icon name="exclamation" size={30} color="#900" /> */}
+    </Card>
+  );
+};
 
 export default Messages;
